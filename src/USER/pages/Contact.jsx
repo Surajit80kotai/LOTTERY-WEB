6c1e4d1cee3e6971425846c69dd9708459758c5d
Surajit Pal <palsurajit06@gmail.cpm>
2023-03-02 23:31:51 +0530

    updateed map size (home)

--- src/USER/pages/Contact.jsx
+++ src/USER/pages/Contact.jsx
@@ -132,7 +132,7 @@ const Contact = () => {
                                         <div className="col-md-4">
                                             <div className="address">
                                                 <div className="map_area">
-                                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117925.21192705746!2d88.27731165151606!3d22.535570756362414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f882db4908f667%3A0x43e330e68f6c2cbc!2sKolkata%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1674557643642!5m2!1sen!2sin" style={{ "width": "390px", "height": "445px", "style": "border:0" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='map'></iframe>
+                                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117925.21192705746!2d88.27731165151606!3d22.535570756362414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f882db4908f667%3A0x43e330e68f6c2cbc!2sKolkata%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1674557643642!5m2!1sen!2sin" style={{ "width": "335px", "height": "400px", "style": "border:0" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='map'></iframe>
                                                 </div>
                                             </div>
                                         </div>
