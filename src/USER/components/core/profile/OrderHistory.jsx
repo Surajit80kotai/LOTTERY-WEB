63df76f142aac7dca47921d29e5fb83731e724fb
Surajit80kotai <surajit80kotai@gmail.com>
2023-03-15 15:29:35 +0530

    Order history modefied.

--- src/USER/components/core/profile/OrderHistory.jsx
+++ src/USER/components/core/profile/OrderHistory.jsx
@@ -90,89 +90,100 @@ const OrderHistory = () => {
                     <div className="content_wrapper">
                         {/* order history  */}
                         <div className="container ">
-                            {/* Filter Dropdown */}
-                            <div className='d-flex justify-content-end mt-4'>
-                                <div className="dropdown">
-                                    <button className="btn btn-outline-dark dropdown-toggle d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i className="ri-equalizer-line" style={{ "fontSize": "18px" ,"margin":"0 5px"}}></i>
-                                        <h2 className='fw-bold'>Filter<i className="fa-solid fa-caret-down mx-2"></i></h2>
-                                    </button>
-                                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" style={{ "width": "180px" }}>
-                                        {/* Clear Filter Button */}
-                                        <li className=' text-center m-3'><button className="btn btn-outline-primary fs-5" onClick={() => filterOrderHistory("")} style={{"borderRadius":"20px"}}>CLEAR FILTER</button></li>
+                            <div className="row">
+                                <div className="col-md-3">
+                                    {/* Filter Dropdown */}
+                                    <div className="filter_area">
+                                        <div className="filter_head">
+                                            <div className="filter_title">
+                                                <h4><span className='filter_icon'><i className="ri-equalizer-line"></i></span>Fillter</h4>
+                                            </div>
+                                            <div className="btn_cancel">
+                                                <button className='btn_clear' onClick={() => filterOrderHistory("")}>Clear</button>
+                                            </div>
+                                        </div>
+                                        <div className="filter_body">
 
-                                        {/* Price Filter Options */}
-                                        <p className='fw-bold fs-4 px-3'>Price:</p>
-                                        <li><Link className="dropdown-item" to="#!" onClick={() => filterOrderHistory("LtoH")} >Low To High</Link></li>
-                                        <li><Link className="dropdown-item" to="#!" onClick={() => filterOrderHistory("HtoL")} >High To Low</Link></li>
-                                        <hr />
+                                            <div className="filter_heading">
+                                                <h5>Price</h5>
+                                            </div>
+                                            <div className="filter_button">
+                                                <Link to="#!" onClick={() => filterOrderHistory("LtoH")}>Low To High</Link>
+                                                <Link to="#!" onClick={() => filterOrderHistory("HtoL")}>High To Low </Link>
+                                            </div>
+                                            <hr />
+                                            <div className="filter_heading">
+                                                <h5>Date</h5>
+                                            </div>
+                                            <div className="filter_button">
+                                                <Link to="#!" onClick={() => filterOrderHistory("NF")}>Newest First</Link>
+                                                <Link to="#!" onClick={() => filterOrderHistory("OF")}>Oldest First </Link>
+                                            </div>
 
-                                        {/* Date Filter Options */}
-                                        <p className='fw-bold fs-4 px-3'>Date:</p>
-                                        <li><Link className="dropdown-item" to="#!" onClick={() => filterOrderHistory("NF")} >Newest First</Link></li>
-                                        <li><Link className="dropdown-item" to="#!" onClick={() => filterOrderHistory("OF")} >Oldest First</Link></li>
-                                    </ul>
+                                        </div>
+                                    </div>
                                 </div>
-                                {/* <button className='d-flex fw-bold'>
-                <img src="/assets/img/filter.png" alt="img" style={{"width":"18px", "height":"18px", "margin":"3px 4px"}} /> <h2 className='fw-bold'>Filter</h2>
-            </button> */}
-                            </div>
-                            <div className="order_histry_wrapper scroll">
-                                {/* order his item  */}
-                                {
-                                    orderHistoryData?.length ?
-                                        filteredOH?.map((item) => {
-                                            return (
-                                                <div className="orderhistroy_item" key={item?._id}>
-                                                    <div className="ribbon-wrapper-green">
-                                                        {
-                                                            item?.is_win === 'true' ?
-                                                                <div className="ribbon-green">Won</div>
-                                                                : null
-                                                        }
-                                                    </div>
+                                <div className="col-md-9">
+                                    <div className="order_histry_wrapper scroll">
+                                        {/* order his item  */}
+                                        {
+                                            orderHistoryData?.length ?
+                                                filteredOH?.map((item) => {
+                                                    return (
+                                                        <div className="orderhistroy_item" key={item?._id}>
+                                                            <div className="ribbon-wrapper-green">
+                                                                {
+                                                                    item?.is_win === 'true' ?
+                                                                        <div className="ribbon-green">Won</div>
+                                                                        : null
+                                                                }
+                                                            </div>
 
-                                                    <div className="pro_im_t">
-                                                        <div className="product_history_img">
-                                                            <img src={baseUrl + item?.image_link} alt="" className="img-fluid" />
-                                                        </div>
-                                                        <div className="info_pro_title">
-                                                            <h4>{item?.ticket_name}</h4>
-                                                            <div className="num_of_tick">
-                                                                <h4>Quantity : {item?.quantity}</h4>
+                                                            <div className="pro_im_t">
+                                                                <div className="product_history_img">
+                                                                    <img src={baseUrl + item?.image_link} alt="" className="img-fluid" />
+                                                                </div>
+                                                                <div className="info_pro_title">
+                                                                    <h4>{item?.ticket_name}</h4>
+                                                                    <div className="num_of_tick">
+                                                                        <h4>Quantity : {item?.quantity}</h4>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div className="info_item">
+                                                                <h3 className="dateofresult"><span></span>Round: {item?.round}</h3>
+                                                            </div>
+                                                            <div className="info_item">
+                                                                <h3 className="dateofresult"><span></span>Price</h3>
+                                                                <p>{userID ? currency_symbol : generalCurrency_symbol}&nbsp;{(item?.total_discount_price)}
+                                                                </p>
+                                                            </div>
+                                                            <div className="info_item">
+                                                                <h3 className="dateofresult"><span></span>Date Of Purchase</h3>
+                                                                <p>{new Date(item?.createdAt).toLocaleString('en-US', {
+                                                                    month: 'short',
+                                                                    day: '2-digit',
+                                                                    year: 'numeric'
+                                                                })}</p>
+                                                            </div>
+                                                            <div className="info_item">
+                                                                <h3 className="dateofresult"><span></span>Date Of Result </h3>
+                                                                <p>{new Date(item?.result_on).toLocaleString('en-US', {
+                                                                    month: 'short',
+                                                                    day: '2-digit',
+                                                                    year: 'numeric'
+                                                                })}</p>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <div className="info_item">
-                                                        <h3 className="dateofresult"><span></span>Round: {item?.round}</h3>
-                                                    </div>
-                                                    <div className="info_item">
-                                                        <h3 className="dateofresult"><span></span>Price</h3>
-                                                        <p>{userID ? currency_symbol : generalCurrency_symbol}&nbsp;{(item?.total_discount_price)}
-                                                        </p>
-                                                    </div>
-                                                    <div className="info_item">
-                                                        <h3 className="dateofresult"><span></span>Date Of Purchase</h3>
-                                                        <p>{new Date(item?.createdAt).toLocaleString('en-US', {
-                                                            month: 'short',
-                                                            day: '2-digit',
-                                                            year: 'numeric'
-                                                        })}</p>
-                                                    </div>
-                                                    <div className="info_item">
-                                                        <h3 className="dateofresult"><span></span>Date Of Result </h3>
-                                                        <p>{new Date(item?.result_on).toLocaleString('en-US', {
-                                                            month: 'short',
-                                                            day: '2-digit',
-                                                            year: 'numeric'
-                                                        })}</p>
-                                                    </div>
-                                                </div>
-                                            )
-                                        })
-                                        : <h1 className='text-center'>No order history present</h1>
-                                }
+                                                    )
+                                                })
+                                                : <h1 className='text-center'>No order history present</h1>
+                                        }
+                                    </div>
+                                </div>
                             </div>
+
+
                             {/* Pagination */}
                             <div className="pagination pagination-style-three">
                                 <ReactPaginate
