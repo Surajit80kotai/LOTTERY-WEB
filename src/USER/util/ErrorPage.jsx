6c18478fd8da987363a933b9b845adaaad542009
Surajit80kotai <surajit80kotai@gmail.com>
2023-04-05 12:56:39 +0530

    error page modefied

--- src/USER/util/ErrorPage.jsx
+++ src/USER/util/ErrorPage.jsx
@@ -4,9 +4,11 @@ const ErrorPage = () => {
 
   return (
     <>
-      <div className="error_msg text-center mt-5">
-        <h1 style={{ "fontSize": "35px", "fontFamily": "sans-serif" }}>Network Error !!!</h1>
-        <p style={{ "fontSize": "25px", "fontFamily": "sans-serif" }}>We are sorry, but the page you requested was not found</p>
+      <div id="error_page_main">
+        <div className="fof">
+          <h1>Error 404</h1>
+          <h2>Page Not Found</h2>
+        </div>
       </div>
     </>
   )
