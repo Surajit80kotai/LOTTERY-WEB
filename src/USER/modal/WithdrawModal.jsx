3323a5e693c5d9bfed6bb73a17cc13811d7600f3
Surajit80kotai <surajit80kotai@gmail.com>
2023-04-11 20:38:17 +0530

    update

--- src/USER/modal/WithdrawModal.jsx
+++ src/USER/modal/WithdrawModal.jsx
@@ -21,7 +21,7 @@ const WithdrawModal = () => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title fs-3" id="exampleModalLabel">Withdraw Money</h5>
+                            <h5 className="modal-title fs-3" id="exampleModalLabel" style={{padding: "0.3rem 15rem"}}>Withdraw Money</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
